265819c8d21681dc3e6829887249aeb1e02f09e1
Lenny Sirivong <lenny@lenny-s.com>
2016-10-05 23:38:43 -0400

    Basic babel config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,13 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
   entry: 'src/scripts/main.js',
   dest: 'build/js/main.min.js',
   format: 'iife',
   sourceMap: 'inline',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
 };
