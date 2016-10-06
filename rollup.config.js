0bc6823358e8dfded77bde42c02bdda826ab90ba
Lenny Sirivong <lenny@lenny-s.com>
2016-10-06 13:24:12 -0400

    env var handling

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
 
 export default {
   entry: 'src/scripts/main.js',
@@ -6,8 +9,18 @@ export default {
   format: 'iife',
   sourceMap: 'inline',
   plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
     babel({
       exclude: 'node_modules/**',
+    }),
+    replace({
+      exclude: 'node_modules/**',
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
   ],
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
+import debug from 'debug';
 import test from './modules/test';
 
 test();
+
+const log = debug('app:log');
+if (ENV !== 'production') {
+  // Enable the logger.
+  debug.enable('*');
+  log('Logging is enabled!');
+} else {
+  debug.disable();
+}
