6e112fc51f72a53382048908b29a76ab4c81ba67
Lenny Sirivong <lenny@lenny-s.com>
2016-10-16 18:12:18 -0400

    Uglify

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from 'rollup-plugin-babel';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'src/scripts/main.js',
@@ -22,5 +23,6 @@ export default {
       exclude: 'node_modules/**',
       ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
+    (process.env.NODE_ENV === 'production' && uglify()),
   ],
 };
