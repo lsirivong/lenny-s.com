2e8ce9445ff736fa3c7d3de406ae6320cf853f98
Lenny Sirivong <lenny@lenny-s.com>
2019-02-03 13:46:57 -0700

    Remove `work` directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/work`,
-        name: `work`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         path: `${__dirname}/content/assets`,
         name: `assets`,
       },
